e7726ad075afc9f3787d8f2a91033d3153f22b77
Lucy Bentham <147002329+LB17GO@users.noreply.github.com>
2025-03-24 14:00:02 +0000

    Renamed column

--- code/Data.py
+++ code/Data.py
@@ -9,12 +9,15 @@ from kagglehub import KaggleDatasetAdapter
 path = "data/song_data.csv"
 df = pd.read_csv(path)
 
-print(df.head())  # First few rows
-print(df.info())  # Column names, data types, and non-null counts
-print(df.describe())  # Summary statistics for numerical columns
-print(df.isnull().sum())  # Count of missing values per column
+#print(df.head())  # First few rows
+#print(df.info())  # Column names, data types, and non-null counts
+#print(df.describe())  # Summary statistics for numerical columns
+#print(df.isnull().sum())  # Count of missing values per column
+
+df = df.rename(columns={'topic': 'genre_cluster'})
 
 df.to_excel("song_data.xlsx", index=False)
+df.to_csv("song_data.csv", index=False)
 
 
 
